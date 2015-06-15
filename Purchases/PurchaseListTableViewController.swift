ce55ac002339f48a785de81bb4b6fcb9e0a9004c
Michael Tremel <itrekkie@gmail.com>
2015-06-15 01:29:36 -0700

    clean memory

--- Purchases/PurchaseListTableViewController.swift
+++ Purchases/PurchaseListTableViewController.swift
@@ -67,7 +67,7 @@ class PurchaseListTableViewController: UITableViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        self.purchases.removeAll()
     }
 
     // MARK: - Table view data source
