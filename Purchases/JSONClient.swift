7fa38f19794208bec1e7de97463314e619d00b2d
Michael Tremel <itrekkie@gmail.com>
2015-06-15 22:14:27 -0700

    refactor

--- Purchases/JSONClient.swift
+++ Purchases/JSONClient.swift
@@ -8,25 +8,33 @@
 
 import UIKit
 
-class JSONClient: NSObject {
-    class func post(url: String, body: String) {
+class JSONClient {
+    class func basicRequest(url: String, method: String, body: String, completionHandler: (NSData?, NSURLResponse?, NSError?) -> Void) {
         let request = NSMutableURLRequest(URL: NSURL(string: url)!)
-        request.HTTPMethod = "POST"
+        request.HTTPMethod = method
         request.HTTPBody = (body as NSString).dataUsingEncoding(NSUTF8StringEncoding)
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
         request.addValue("application/json", forHTTPHeaderField: "Accept")
         
-        NSURLSession.sharedSession().dataTaskWithRequest(request, completionHandler: {data, response, error -> Void in })!.resume()
+        NSURLSession.sharedSession().dataTaskWithRequest(request, completionHandler: completionHandler)!.resume()
+    }
+    
+    class func post(url: String, body: String) {
+        basicRequest(url, method: "POST", body: body, completionHandler: {(_, _, _) in })
     }
     
     class func get(url: String, callback: ((AnyObject) -> Void)) {
-        NSURLSession.sharedSession().dataTaskWithURL(NSURL(string: url)!, completionHandler: {(data, response, error) -> Void in
+        basicRequest(url, method: "GET", body: "") { (data, response, error) in
             do {
                 if let json = data {
                     let parsed = try NSJSONSerialization.JSONObjectWithData(json, options: NSJSONReadingOptions.AllowFragments)
                     callback(parsed)
                 }
             } catch _ { }
-        })!.resume()
+        }
+    }
+    
+    class func delete(url: String) {
+        basicRequest(url, method: "DELETE", body: "", completionHandler: {(_, _, _) in })
     }
 }
